536684be803021e6f078490c3c309cc8003ac5cd
Mark Delchev <markdelchev@gmail.com>
2022-09-07 20:13:46 +0300

    Better image examples

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# face-detection
-A face detection app.
+# Face detection app
+## Main features
+- Detect multiple faces in an image
+- Detect if there are no faces in an image
+## Technologies used
+- This app is made with the OpenCV Computer Vision Library
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import cv2
 
 trained_face_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-img = cv2.imread("image4.jpg")
+img = cv2.imread("image2.jpg")
 grayscaled_img = cv2.cvtColor( img, cv2.COLOR_BGR2GRAY)
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
-print(face_coordinates)
+#print(face_coordinates)
+
 for (x, y, w, h) in face_coordinates:
     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
 if face_coordinates == ():
-    print("There is no face in this image")
+    print("There isn't a face in this image.")
 else:
     cv2.imshow("Face detection", img)
     cv2.waitKey()
+
 print("Code Completed")
