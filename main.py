a7e1a8bdbb5c20567c2271e873427f2ee3ed0042
Mark Delchev <markdelchev@gmail.com>
2022-09-07 19:20:15 +0300

    image conversion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 trained_face_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-img = cv2.imread("image1.jpeg")
-cv2.imshow("Elon Musk", img)
+img = cv2.imread("image2.jpg")
+grayscaled_img = cv2.cvtColor( img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("Boyko", grayscaled_img)
 cv2.waitKey()
 print("Code Completed")
