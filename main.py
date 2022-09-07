1fbe70c6470f76035ee3a407f5b4a40ae83dd4cc
Mark Delchev <markdelchev@gmail.com>
2022-09-07 19:40:07 +0300

    face detection green rectangle

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import cv2
 trained_face_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 img = cv2.imread("image2.jpg")
 grayscaled_img = cv2.cvtColor( img, cv2.COLOR_BGR2GRAY)
-
-cv2.imshow("Boyko", grayscaled_img)
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+print(face_coordinates)
+cv2.rectangle(img, (80, 62), (80+191, 62+191), (0, 255, 0), 2)
+cv2.imshow("Boyko", img)
 cv2.waitKey()
 print("Code Completed")
